125fc31646d9fac9f9bac0be27ce1af9608df351
MarcoNaw <166357601+MarcoNawr@users.noreply.github.com>
2024-10-29 23:24:06 +0100

    add: display notes

---
+++ script.js
@@ -0,0 +1,13 @@
+let notes = ['Äpfel', 'Bannanen', 'Birnen'];
+
+function renderNotes() {
+    let contentRef = document.getElementById('content');
+    contentRef.innerHTML = notes;
+}
+
+
+
+
+// notizen hinzufügen
+// notizen löschen
+// notizen archivieren
