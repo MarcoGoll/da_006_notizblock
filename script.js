bcac8315f87a85d55929474c848d03c4c71479f3
MarcoNaw <166357601+MarcoNawr@users.noreply.github.com>
2024-10-30 09:27:08 +0100

    add: Add Note

--- script.js
+++ script.js
@@ -2,12 +2,25 @@ let notes = ['Äpfel', 'Bannanen', 'Birnen'];
 
 function renderNotes() {
     let contentRef = document.getElementById('content');
-    contentRef.innerHTML = notes;
+    contentRef.innerHTML = '';
+    for (let i = 0; i < notes.length; i++) {
+        const note = notes[i];
+        contentRef.innerHTML += getNoteTemplate(note);
+    }
 }
 
+function getNoteTemplate(note) {
+    return `<p class="note">+ ${note}</p>`;
+}
 
+function addNote() {
+    let noteInputRef = document.getElementById('inpNewNote');
+    let noteInput = noteInputRef.value;
 
+    notes.push(noteInput);
+    renderNotes();
+    noteInputRef.value = '';
+}
 
-// notizen hinzufügen
 // notizen löschen
 // notizen archivieren
