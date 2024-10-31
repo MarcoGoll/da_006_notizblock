aebd15dc697969d59b0b759cc6ad5ec26f3d0e34
MarcoNaw <166357601+MarcoNawr@users.noreply.github.com>
2024-10-31 20:53:10 +0100

    add: save/load local storage

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let noteTitles = ["Obst", "Obst", "Gemüse"];
-let notes = ['Äpfel', 'Bannanen', 'Möhren'];
+let noteTitles = [];
+let notes = [];
 
 let trashNoteTitles = [];
 let trashNotes = [];
 
+
+function init() {
+    loadFromLocalStorage();
+    renderNotes();
+    renderTrashNotes();
+}
 
 function renderNotes() {
     let contentRef = document.getElementById('content');
@@ -32,10 +38,14 @@ function getTrashNoteTemplate(i) {
 function addNote() {
     let noteInputRef = document.getElementById('inpNewNote');
     let noteInput = noteInputRef.value;
+    let noteInputTitleRef = document.getElementById('inpNewNoteTitle');
+    let noteInputTitle = noteInputTitleRef.value;
 
+    noteTitles.push(noteInputTitle);
     notes.push(noteInput);
     renderNotes();
     noteInputRef.value = '';
+    noteInputTitleRef.value = '';
 }
 
 function trashNote(index) {
@@ -50,6 +60,29 @@ function trashNote(index) {
 function deleteNote(index) {
     trashNotes.splice(index, 1);
     renderTrashNotes();
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem("noteTitles", JSON.stringify(noteTitles)); /*JSON.stringify(myData) ==> Array wird als string abgespeichert, da die Funktion ein String verlangt TODO: doku lesen / in Dev-Knowledge aufnehmen*/
+    localStorage.setItem("notes", JSON.stringify(notes));
+    localStorage.setItem("trashNoteTitles", JSON.stringify(trashNoteTitles));
+    localStorage.setItem("trashNotes", JSON.stringify(trashNotes));
+}
+
+function loadFromLocalStorage() {
+    let loadedNoteTitle = JSON.parse(localStorage.getItem("noteTitles"));
+    let loadedNote = JSON.parse(localStorage.getItem("notes"));
+    let loadedTrashNoteTitle = JSON.parse(localStorage.getItem("trashNoteTitles"));
+    let loadedTrashNote = JSON.parse(localStorage.getItem("trashNotes"));
+
+    if (loadedNoteTitle == null || loadedNote == null || loadedTrashNoteTitle == null || loadedTrashNote == null) {
+        console.info("Mindestens eines der local gespeicherten Notiz-Daten ist nicht vorhanden. Die Notizen wurden daher nicht geladen.");
+    } else {
+        noteTitles = loadedNoteTitle;
+        notes = loadedNote;
+        trashNoteTitles = loadedTrashNoteTitle;
+        trashNotes = loadedTrashNote;
+    }
 }
 
 // notizen archivieren
